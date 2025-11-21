f7c5c65c2d82739e7b96e1e733c6b56deb627762
Debananda.Mohanty <todebananda@hotmail.com>
2025-11-21 21:57:31 +0530

    minor corrections done

--- routes/gemini.py
+++ routes/gemini.py
@@ -15,18 +15,18 @@ ai = Client()
 router = APIRouter()
 
 @router.post("/generateImage")
-async def generate_image(productImages: Annotated[list[str], Form()], 
-                         userImage: Annotated[UploadFile, File()]):
+async def generate_image(product_images: Annotated[list[str], Form()],
+                         user_pic: Annotated[UploadFile, File()]):
     try:
-        user_image_content = await userImage.read()
+        user_image_content = await user_pic.read()
         user_image = Image.open(io.BytesIO(user_image_content))
         current_image_base64 = user_image_content
         current_image_mime_type = Image.MIME.get(user_image.format or "image/png")
-        for product_image_url in productImages:
+        for product_image_url in product_images:
             async with aiohttp.ClientSession() as session:
                 async with session.get(product_image_url) as resp:
                     product_image_content = await resp.read()
-                    product_image_mime_type = resp.headers.get("Content-Type", "image/png")
+                    product_image_mime_type = resp.content_type
 
             product_part = Part(
                 inline_data=Blob(
@@ -41,16 +41,16 @@ async def generate_image(productImages: Annotated[list[str], Form()],
                 )
             )
             text_part = Part(
-                text_data=image_prompt_text.format("Ribbon Joy Pullover")
+                text=image_prompt_text.format("Ribbon Joy Pullover")
             )     
             response = ai.models.generate_content(
-                model="gemini-2.5-flash_image",
+                model="gemini-2.5-flash-image",
                 contents=Content(parts=[person_part, product_part, text_part]),
                 config=GenerateContentConfig(response_modalities=["IMAGE"]),
             )
             if response.candidates and len(response.candidates) > 0:
                 generated_image = response.candidates[0]
-                if(generated_image.content and generated_image.content.parts):
+                if generated_image.content and generated_image.content.parts:
                     for part in generated_image.content.parts:
                         if part.inline_data:
                             current_image_base64 = part.inline_data.data
